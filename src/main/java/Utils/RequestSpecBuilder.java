6eeaade7c2b586a7db0a928393dbb5a469361e6b
Ayush Agrawal <ayushagrawal@Ayushs-MacBook-Pro.local>
2021-09-05 00:21:22 +0530

    Modified RequestSpecBuilder

--- src/main/java/Utils/RequestSpecBuilder.java
+++ src/main/java/Utils/RequestSpecBuilder.java
@@ -6,7 +6,6 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 
-import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 public final class RequestSpecBuilder {
@@ -16,17 +15,21 @@ public final class RequestSpecBuilder {
     private RequestSpecBuilder() {
     }
 
-    public static void initRequestSpec(String baseURL) throws FileNotFoundException {
-        PrintStream logFile = new PrintStream(FilePaths.getLogFilePath());
-        logFile.flush();
-        requestSpecification =
-                RestAssured
-                        .given()
-                        .baseUri(baseURL)
-                        .header("Content-Type", "application/json")
-                        .urlEncodingEnabled(false)
-                        .filter(RequestLoggingFilter.logRequestTo(logFile))
-                        .filter(ResponseLoggingFilter.logResponseTo(logFile));
+    public static void initRequestSpec(String baseURL) {
+        try {
+            PrintStream logFile = new PrintStream(FilePaths.getLogFilePath());
+            logFile.flush();
+            requestSpecification =
+                    RestAssured
+                            .given()
+                            .baseUri(baseURL)
+                            .header("Content-Type", "application/json")
+                            .urlEncodingEnabled(false)
+                            .filter(RequestLoggingFilter.logRequestTo(logFile))
+                            .filter(ResponseLoggingFilter.logResponseTo(logFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static RequestSpecification getRequestSpec() {
